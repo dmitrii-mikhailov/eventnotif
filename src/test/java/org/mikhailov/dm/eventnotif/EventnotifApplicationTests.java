7bf03eb9d2a303156c3aeb2b1ff0943de2b6592a
Dmitrii Mikhailov <dmitriy.mikhailoff@gmail.com>
2025-04-26 12:38:38 +0300

    init

---
+++ src/test/java/org/mikhailov/dm/eventnotif/EventnotifApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mikhailov.dm.eventnotif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventnotifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
